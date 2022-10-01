288d05f582811fb8a503c22aeed32e4210211956
sahot <114588437+ssshotakaoka@users.noreply.github.com>
2022-10-01 23:39:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ add_my_furuit = streamlit.text_input('What fruit would you like to add?','jackfr
 streamlit.write('Thanks for adding ', add_my_furuit)
 
 #This will not work correctly, but just go with it for now
-my_car.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
